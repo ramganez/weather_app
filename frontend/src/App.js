222baadb541903aac7dcf844864c07851af08de8
ramganez <ramganesh7k@gmail.com>
2022-10-11 07:29:15 -0400

    Adding query component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import HeaderComponent from './components/Header';
+import QueryComponent from './components/Query';
 
 function App() {
 
   return (
     <div className="App">
       <HeaderComponent></HeaderComponent>
+      <QueryComponent></QueryComponent>
     </div>
   );
 }
