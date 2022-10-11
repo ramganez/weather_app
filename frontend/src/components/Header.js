1362707d915fa587395bd27c3cf46e37a19e7138
ramganez <ramganesh7k@gmail.com>
2022-10-10 23:38:03 -0400

    frontend - adding header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import Typography from '@material-ui/core/Typography'
+
+export default function HeaderComponent() {
+  return (
+    < header className="App-header" >
+      <Typography align="center" variant="h2" component="h2" gutterBottom>
+        Weather
+      </Typography>
+    </header >
+  );
+}
